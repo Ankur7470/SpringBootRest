38ca08b0b460ea5832d1c2428fd11587359886da
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-02 17:20:46 +0530

    Updated SpringBootRestApplication.java main file

--- src/main/java/com/ankursinha/springbootrest/SpringBootRestApplication.java
+++ src/main/java/com/ankursinha/springbootrest/SpringBootRestApplication.java
@@ -2,8 +2,9 @@ package com.ankursinha.springbootrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class SpringBootRestApplication {
 
     public static void main(String[] args) {
