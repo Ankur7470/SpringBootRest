1557ca55d96d64d09f489ffea78250dbdaa4e693
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-02 19:46:58 +0530

    Updated CustomerController

--- src/main/java/com/ankursinha/springbootrest/controller/CustomerController.java
+++ src/main/java/com/ankursinha/springbootrest/controller/CustomerController.java
@@ -1,4 +1,36 @@
 package com.ankursinha.springbootrest.controller;
 
+import com.ankursinha.springbootrest.dto.CustomerRequest;
+import com.ankursinha.springbootrest.dto.CustomerResponse;
+import com.ankursinha.springbootrest.dto.LoginRequest;
+import com.ankursinha.springbootrest.dto.LoginResponse;
+import com.ankursinha.springbootrest.service.CustomerService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/api/v1/customer")
 public class CustomerController {
+
+    private final CustomerService customerService;
+
+    @PostMapping("/create")
+    public ResponseEntity<String> createCustomer(@RequestBody @Valid CustomerRequest request) {
+        return ResponseEntity.ok(customerService.createCustomer(request));
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<String> testEndpoint() {
+        System.out.println("Test endpoint hit!");
+        return ResponseEntity.ok("Test successful!");
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody @Valid LoginRequest request) {
+        LoginResponse response = customerService.login(request);
+        return ResponseEntity.ok(response);
+    }
 }
