e4301b90a05641cdab0006b7bec8b95305274cba
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-02 19:46:17 +0530

    Updated CustomerMapper

--- src/main/java/com/ankursinha/springbootrest/mapper/CustomerMapper.java
+++ src/main/java/com/ankursinha/springbootrest/mapper/CustomerMapper.java
@@ -1,4 +1,42 @@
 package com.ankursinha.springbootrest.mapper;
 
+import com.ankursinha.springbootrest.dto.CustomerRequest;
+import com.ankursinha.springbootrest.dto.CustomerResponse;
+import com.ankursinha.springbootrest.entity.Customer;
+import org.springframework.stereotype.Component;
+
+@Component
 public class CustomerMapper {
+    public Customer toEntity(CustomerRequest request) {
+        Customer customer = new Customer();
+        customer.setFirstName(request.firstName());
+        customer.setLastName(request.lastName());
+        customer.setEmail(request.email());
+        customer.setPassword(request.password());
+        return customer;
+    }
 }
+
+//@Component
+//public class CustomerMapper {
+//
+//    // Convert CustomerRequest to Customer entity
+//    public Customer toEntity(CustomerRequest request) {
+//        return new Customer(
+//                null, // ID will be generated automatically
+//                request.firstName(),
+//                request.lastName(),
+//                request.email(),
+//                request.password()
+//        );
+//    }
+//
+//    // Convert Customer entity to CustomerResponse
+//    public CustomerResponse toResponse(Customer customer) {
+//        return new CustomerResponse(
+//                customer.getFirstName(),
+//                customer.getLastName(),
+//                customer.getEmail()
+//        );
+//    }
+//}
