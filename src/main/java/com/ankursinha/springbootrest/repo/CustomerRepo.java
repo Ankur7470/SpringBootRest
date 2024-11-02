b560bcbe96dae3cab7c0840e015db77e693206da
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-02 19:45:58 +0530

    Updated CustomerRepo

--- src/main/java/com/ankursinha/springbootrest/repo/CustomerRepo.java
+++ src/main/java/com/ankursinha/springbootrest/repo/CustomerRepo.java
@@ -1,4 +1,11 @@
 package com.ankursinha.springbootrest.repo;
 
-public interface CustomerRepo {
+
+import com.ankursinha.springbootrest.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
+    Optional<Customer> findByEmail(String email);
 }
