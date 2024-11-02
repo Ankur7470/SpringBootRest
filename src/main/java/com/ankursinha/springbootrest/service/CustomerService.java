1eb1d07bafa2fa21b133194b2738f3e3de73b8b0
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-02 19:46:42 +0530

    Updated CustomerService

--- src/main/java/com/ankursinha/springbootrest/service/CustomerService.java
+++ src/main/java/com/ankursinha/springbootrest/service/CustomerService.java
@@ -1,4 +1,38 @@
 package com.ankursinha.springbootrest.service;
 
+import com.ankursinha.springbootrest.dto.CustomerRequest;
+import com.ankursinha.springbootrest.dto.CustomerResponse;
+import com.ankursinha.springbootrest.dto.LoginRequest;
+import com.ankursinha.springbootrest.dto.LoginResponse;
+import com.ankursinha.springbootrest.entity.Customer;
+import com.ankursinha.springbootrest.mapper.CustomerMapper;
+import com.ankursinha.springbootrest.repo.CustomerRepo;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CustomerService {
+
+    private final CustomerRepo repo;
+    private final CustomerMapper mapper;
+
+    public String createCustomer(CustomerRequest request) {
+        Customer customer = mapper.toEntity(request);
+        repo.save(customer);
+        return "Customer Created";
+    }
+
+    public LoginResponse login(LoginRequest request) {
+        Optional<Customer> customerOpt = repo.findByEmail(request.email());
+
+        if (customerOpt.isPresent() && customerOpt.get().getPassword().equals(request.password())) {
+            return new LoginResponse("Login successful");
+        } else {
+            throw new RuntimeException("Invalid email or password");
+        }
+    }
 }
