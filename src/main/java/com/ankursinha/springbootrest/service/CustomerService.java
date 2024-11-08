e9c7d24136e9aa57e9b929bd667a3576393fad5a
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-08 11:37:33 +0530

    Added Retrieve Customer By Email Service

--- src/main/java/com/ankursinha/springbootrest/service/CustomerService.java
+++ src/main/java/com/ankursinha/springbootrest/service/CustomerService.java
@@ -5,6 +5,7 @@ import com.ankursinha.springbootrest.dto.CustomerResponse;
 import com.ankursinha.springbootrest.dto.LoginRequest;
 import com.ankursinha.springbootrest.dto.LoginResponse;
 import com.ankursinha.springbootrest.entity.Customer;
+import com.ankursinha.springbootrest.exception.CustomerNotFoundException;
 import com.ankursinha.springbootrest.mapper.CustomerMapper;
 import com.ankursinha.springbootrest.repo.CustomerRepo;
 import jakarta.validation.Valid;
@@ -12,6 +13,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
@@ -24,6 +27,14 @@ public class CustomerService {
         Customer customer = mapper.toEntity(request);
         Customer savedCustomer = repo.save(customer);
         return mapper.toDto(savedCustomer);
+    }
+
+    public CustomerResponse retrieveCustomer(String email) {
+        Customer customer = repo.findByEmail(email)
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        format("Cannot update Customer:: No customer found with the provided ID:: %s", email)
+                ));
+        return mapper.toDto(customer);
     }
 
     public LoginResponse login(LoginRequest request) {
