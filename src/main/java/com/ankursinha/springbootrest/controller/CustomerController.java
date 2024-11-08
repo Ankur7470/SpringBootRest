2a5b655ba5efcfe50850f31b5412426d2ff4c89d
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-08 17:44:59 +0530

    Created Endpoint for Update and Delete Customer

--- src/main/java/com/ankursinha/springbootrest/controller/CustomerController.java
+++ src/main/java/com/ankursinha/springbootrest/controller/CustomerController.java
@@ -21,15 +21,31 @@ public class CustomerController {
         return ResponseEntity.ok("Test successful!");
     }
 
+    //Endpoint to create a new Customer
     @PostMapping("/create")
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody @Valid CustomerRequest request) {
         CustomerResponse response = customerService.createCustomer(request);
         return ResponseEntity.ok(response);
     }
 
+    //Endpoint to get customer detail
     @GetMapping("/{email}")
     public ResponseEntity<CustomerResponse> getCustomer(@PathVariable("email") String email) {
         CustomerResponse response = customerService.retrieveCustomer(email);
         return ResponseEntity.ok(response);
+    }
+
+    // Endpoint to update customer details
+    @PutMapping("/{id}")
+    public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable("id") Long id, @RequestBody @Valid CustomerRequest request) {
+        CustomerResponse response = customerService.updateCustomer(id, request);
+        return ResponseEntity.ok(response);
+    }
+
+    // Endpoint to delete a customer
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCustomer(@PathVariable("id") Long id) {
+        String message = customerService.deleteCustomer(id);
+        return ResponseEntity.ok(message);
     }
 }
