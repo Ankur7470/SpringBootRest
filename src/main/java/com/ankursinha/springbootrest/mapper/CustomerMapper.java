1eba5dae664d617a88484d9db236ac6a18731226
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-08 11:04:44 +0530

    Added Mapper for Customer -> Customer Response

--- src/main/java/com/ankursinha/springbootrest/mapper/CustomerMapper.java
+++ src/main/java/com/ankursinha/springbootrest/mapper/CustomerMapper.java
@@ -5,8 +5,11 @@ import com.ankursinha.springbootrest.dto.CustomerResponse;
 import com.ankursinha.springbootrest.entity.Customer;
 import org.springframework.stereotype.Component;
 
+import static org.springframework.data.jpa.domain.AbstractPersistable_.id;
+
 @Component
 public class CustomerMapper {
+
     public Customer toEntity(CustomerRequest request) {
         Customer customer = new Customer();
         customer.setFirstName(request.firstName());
@@ -15,28 +18,12 @@ public class CustomerMapper {
         customer.setPassword(request.password());
         return customer;
     }
-}
 
-//@Component
-//public class CustomerMapper {
-//
-//    // Convert CustomerRequest to Customer entity
-//    public Customer toEntity(CustomerRequest request) {
-//        return new Customer(
-//                null, // ID will be generated automatically
-//                request.firstName(),
-//                request.lastName(),
-//                request.email(),
-//                request.password()
-//        );
-//    }
-//
-//    // Convert Customer entity to CustomerResponse
-//    public CustomerResponse toResponse(Customer customer) {
-//        return new CustomerResponse(
-//                customer.getFirstName(),
-//                customer.getLastName(),
-//                customer.getEmail()
-//        );
-//    }
-//}
+    public CustomerResponse toDto(Customer customer) {
+        return new CustomerResponse(
+                customer.getFirstName(),
+                customer.getLastName(),
+                customer.getEmail()
+        );
+    }
+}
