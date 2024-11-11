81ea215166c24c730f9e38e1a8b6a2220de74db5
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-11 21:08:21 +0530

    Updated Controller To send Https status Also

--- src/main/java/com/ankursinha/springbootrest/controller/CustomerController.java
+++ src/main/java/com/ankursinha/springbootrest/controller/CustomerController.java
@@ -4,6 +4,7 @@ import com.ankursinha.springbootrest.dto.CustomerRequest;
 import com.ankursinha.springbootrest.dto.CustomerResponse;
 import com.ankursinha.springbootrest.service.CustomerService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,27 +26,28 @@ public class CustomerController {
     @PostMapping("/create")
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody @Valid CustomerRequest request) {
         CustomerResponse response = customerService.createCustomer(request);
-        return ResponseEntity.ok(response);
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     //Endpoint to get customer detail
     @GetMapping("/{email}")
-    public ResponseEntity<CustomerResponse> getCustomer(@PathVariable("email") String email) {
+    public ResponseEntity<CustomerResponse> getCustomer(@PathVariable String email) {
         CustomerResponse response = customerService.retrieveCustomer(email);
-        return ResponseEntity.ok(response);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // Endpoint to update customer details
     @PutMapping("/{id}")
-    public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable("id") Long id, @RequestBody @Valid CustomerRequest request) {
+    public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable Long id, @Valid @RequestBody CustomerRequest request) {
         CustomerResponse response = customerService.updateCustomer(id, request);
-        return ResponseEntity.ok(response);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
+
 
     // Endpoint to delete a customer
     @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCustomer(@PathVariable("id") Long id) {
-        String message = customerService.deleteCustomer(id);
-        return ResponseEntity.ok(message);
+    public ResponseEntity<String> deleteCustomer(@PathVariable Long id) {
+        String response = customerService.deleteCustomer(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
