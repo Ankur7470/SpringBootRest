bddd63c40066de8109dd03322e388f3ab623fefe
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-11 21:17:32 +0530

    Updated auth service to return JWT Token on login

--- src/main/java/com/ankursinha/springbootrest/service/AuthService.java
+++ src/main/java/com/ankursinha/springbootrest/service/AuthService.java
@@ -1,28 +1,38 @@
 package com.ankursinha.springbootrest.service;
 
 import com.ankursinha.springbootrest.dto.LoginRequest;
-import com.ankursinha.springbootrest.dto.LoginResponse;
+//import com.ankursinha.springbootrest.dto.LoginResponse;
 import com.ankursinha.springbootrest.entity.Customer;
+import com.ankursinha.springbootrest.exception.CustomerNotFoundException;
+import com.ankursinha.springbootrest.helper.EncryptionService;
+import com.ankursinha.springbootrest.helper.JWTHelper;
 import com.ankursinha.springbootrest.repo.CustomerRepo;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+//import java.util.Optional;
+
+import static java.lang.String.format;
 
 @Service
 @RequiredArgsConstructor
 public class AuthService {
 
     private final CustomerRepo repo;
+    private final EncryptionService encryptionService;
+    private final JWTHelper jwtHelper;
 
-    public LoginResponse login(LoginRequest request) {
-        Optional<Customer> customerOpt = repo.findByEmail(request.email());
+    public String login(LoginRequest request) {
+        Customer customer = repo.findByEmail(request.email())
+                .orElseThrow(() -> new CustomerNotFoundException(
+                        format("Cannot update Customer:: No customer found with the provided ID:: %s", request.email())
+                ));
 
-        if (customerOpt.isPresent() && customerOpt.get().getPassword().equals(request.password())) {
-            return new LoginResponse("Login successful");
-        } else {
-            throw new RuntimeException("Invalid email or password");
+        if(!encryptionService.validates(request.password(), customer.getPassword())) {
+            return "Wrong Password or Email";
         }
+
+        return jwtHelper.generateToken(request.email());
     }
 }
