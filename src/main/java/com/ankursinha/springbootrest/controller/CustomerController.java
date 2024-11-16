64515609e14f507c5ae145c5d6d546aba7d667a7
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-16 15:31:36 +0530

    Transferred Login Endpoint, Changed the name of Create endpoint to register

--- src/main/java/com/ankursinha/springbootrest/controller/CustomerController.java
+++ src/main/java/com/ankursinha/springbootrest/controller/CustomerController.java
@@ -2,20 +2,23 @@ package com.ankursinha.springbootrest.controller;
 
 import com.ankursinha.springbootrest.dto.CustomerRequest;
 import com.ankursinha.springbootrest.dto.CustomerResponse;
+import com.ankursinha.springbootrest.dto.LoginRequest;
 import com.ankursinha.springbootrest.service.CustomerService;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
-@RestController
-@RequestMapping("/api/v1/customer")
+@RestController()
 public class CustomerController {
 
+    @Autowired
     private final CustomerService customerService;
 
+    //endpoint for testing
     @GetMapping("/test")
     public ResponseEntity<String> testEndpoint() {
         System.out.println("Test endpoint hit!");
@@ -23,10 +26,17 @@ public class CustomerController {
     }
 
     //Endpoint to create a new Customer
-    @PostMapping("/create")
+    @PostMapping("/register")
     public ResponseEntity<CustomerResponse> createCustomer(@RequestBody @Valid CustomerRequest request) {
-        CustomerResponse response = customerService.createCustomer(request);
-        return new ResponseEntity<>(response, HttpStatus.CREATED);
+        CustomerResponse response = customerService.register(request);
+        return ResponseEntity.ok(response);
+    }
+
+    //Endpoint for Login of a customer
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid LoginRequest request) {
+        String response = customerService.login(request);
+        return ResponseEntity.ok(response);
     }
 
     //Endpoint to get customer detail
@@ -36,18 +46,18 @@ public class CustomerController {
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
-    // Endpoint to update customer details
-    @PutMapping("/{id}")
-    public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable Long id, @Valid @RequestBody CustomerRequest request) {
-        CustomerResponse response = customerService.updateCustomer(id, request);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
-
-
-    // Endpoint to delete a customer
-    @DeleteMapping("/{id}")
-    public ResponseEntity<String> deleteCustomer(@PathVariable Long id) {
-        String response = customerService.deleteCustomer(id);
-        return new ResponseEntity<>(response, HttpStatus.OK);
-    }
+// //   Endpoint to update customer details
+//    @PutMapping("/{id}")
+//    public ResponseEntity<CustomerResponse> updateCustomer(@PathVariable Long id, @Valid @RequestBody CustomerRequest request) {
+//        CustomerResponse response = customerService.updateCustomer(id, request);
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//    }
+//
+//
+// //   Endpoint to delete a customer
+//    @DeleteMapping("/{id}")
+//    public ResponseEntity<String> deleteCustomer(@PathVariable Long id) {
+//        String response = customerService.deleteCustomer(id);
+//        return new ResponseEntity<>(response, HttpStatus.OK);
+//    }
 }
