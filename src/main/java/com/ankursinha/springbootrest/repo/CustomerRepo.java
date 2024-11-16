d8fff667317cdd6736b3f0133189d4ac94d1b210
Ankur Sinha <ankur.sinha2019@gmail.com>
2024-11-16 15:46:54 +0530

    Changed Return Type of findByEmail()
    Changed from  Optional<Customer> to Customer

--- src/main/java/com/ankursinha/springbootrest/repo/CustomerRepo.java
+++ src/main/java/com/ankursinha/springbootrest/repo/CustomerRepo.java
@@ -1,14 +1,11 @@
 package com.ankursinha.springbootrest.repo;
 
-
 import com.ankursinha.springbootrest.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
+@Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
-    Optional<Customer> findByEmail(String email);
-    Optional<Customer> findById(Long id);
-
+    Customer findByEmail(String email);
 }
 
